0811926f6c846131c18023ec6eee142fa6ed139f
Vitor Costa <vitor.costa@exaud.com>
2022-03-07 15:45:49 +0000

    Numeric separators

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -339,3 +339,19 @@ labelBalance.addEventListener('click', function () {
     if (i % 2 !== 0) row.style.backgroundColor = 'blue';
   });
 });
+
+//Numeric Separator _
+const diameter = 287_460_000_000;
+//usually we write it as 287.460.000.000
+console.log(diameter);
+const priceCents = 345_99;
+console.log(priceCents);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.14_15;
+console.log(PI);
+
+console.log(Number('230_000')); //NaN
+console.log(Number.parseInt('230_000'));
