78e60acd39cd79788e026b93dffd52d0f515c798
Vitor Costa <vitor.costa@exaud.com>
2022-03-07 14:36:33 +0000

    Math and rounding numbers

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -278,3 +278,45 @@ console.log(Number.isFinite(20));
 console.log(Number.isFinite('20'));
 console.log(Number.isFinite(+'20'));
 console.log(Number.isFinite(23 / 0));
+
+//Math and Rounding
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+//get max
+console.log(Math.max(5, 18, '23', 11, 2));
+//doesn't parse
+console.log(Math.max(5, 18, '23px', 11, 2));
+//get min
+console.log(Math.min(5, 18, '23', 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+//returns a ramdom nr between the 2 given
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+console.log(randomInt(10, 20));
+
+//Rounding integers
+console.log(Math.trunc(23.3));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+//rounds up
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+// rounds down
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
+//different on negatives
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.3));
+
+//rounding decimals
+console.log((2, 7).toFixed(0));
+console.log((2, 7).toFixed(2));
+console.log((2, 345).toFixed(5));
+//return nr not string
+console.log(+(2, 345).toFixed(2));
