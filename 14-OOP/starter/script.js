ea95224fbc696da3e00ae1e6e10547dfd1e34ef6
Vitor Costa <vitor.costa@exaud.com>
2022-03-11 12:41:15 +0000

    Prototype Intro

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// Constructor fn start with caps (conventioned) = prototype
+const Person = function (firtsName, birthYear) {
+  // fn Declaration or expressions, NO ARROW fn
+  // Instance properties
+  this.firstName = firtsName;
+  this.birthYear = birthYear;
+  // never declare a fn inside the constructor - bad practice
+  //   this.calcAge = function(){
+  //       console.log(2037 - this.birthYear);
+  //   }
+};
+
+const vitor = new Person('Vitor', 1983);
+console.log(vitor);
+
+// 1. new Obj {} is created
+// 2. fn is called, this = {}
+// 3. the {} is linked to the prototype
+// 4. fn automaticaly returns {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(vitor instanceof Person); // true/ false
