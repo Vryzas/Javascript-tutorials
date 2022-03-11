150445d7ee5adce9a874f7b8c329d327f9bbde63
Vitor Costa <vitor.costa@exaud.com>
2022-03-11 16:41:09 +0000

    coding challenge 1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -73,3 +73,37 @@ console.log(arr.unique());
 const h1 = document.querySelector('h1');
 console.dir(h1.__proto__); // Prototype chain = HTMLElement => Element => Node => EventTarget => Object
 console.dir(x => x + 1); // fns ultimate prototype will always be obj
+
+////////////////Coding Challenge/////////////////
+
+//1. Use a constructor function to implementa 'Car'. A car has a 'make' and a 'speed' property. The 'speed' property is the current speed of the car in km/h
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+  //   console.log(`${this.make} going at ${this.speed} km/h`);
+};
+
+//2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} going at ${this.speed} km/h`);
+};
+
+//3. Implement a 'brake' method that will decrease the car's speed by 5,and log the new speed to the console
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} going at ${this.speed} km/h`);
+};
+
+//4. Create 2 'Car' objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+car1.accelerate();
+car2.accelerate();
+car1.brake();
+car2.brake();
+car1.brake();
+car1.brake();
+car1.brake();
+car1.brake();
