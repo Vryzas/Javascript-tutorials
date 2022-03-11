b2971971310395416fb665883b6faef6f86afd72
Vitor Costa <vitor.costa@exaud.com>
2022-03-11 14:18:37 +0000

    Prototype inheritance

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -25,3 +25,29 @@ const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
 console.log(vitor instanceof Person); // true/ false
+
+// Prototypes
+console.log(Person.prototype);
+
+// fn declared outside constructor, this way only 1 copy of the fn is created
+// serving all objs of the prototype instead of 1 copy per prototype
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+vitor.calcAge();
+matilda.calcAge();
+
+console.log(vitor.__proto__); // shows the obj prototype
+console.log(vitor.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(vitor)); // true
+console.log(Person.prototype.isPrototypeOf(matilda)); // true
+console.log(Person.prototype.isPrototypeOf(Person)); // false
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(vitor.species, matilda.species);
+
+console.log(vitor.hasOwnProperty('firstName')); //true
+// 'species' is a class atribute and not a obj atribute
+console.log(vitor.hasOwnProperty('species')); //false
