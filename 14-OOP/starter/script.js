575ce987dfaeea84062d5fbd222a34b46dac7fe6
Vitor Costa <vitor.costa@exaud.com>
2022-03-14 11:00:35 +0000

    Object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -195,3 +195,23 @@ Person.hey = function () {
 Person.hey();
 
 PersonCl.hey();
+
+// Object.create
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2000;
+steven.calcAge();
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
