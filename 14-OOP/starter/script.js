10b963f4c3dd2eb8205535bd158f56551920e6d1
Vitor Costa <vitor.costa@exaud.com>
2022-03-14 10:42:54 +0000

    getters and setters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -112,8 +112,8 @@ car1.brake();
 
 class PersonCl {
   //=== 2 const PersonCL = function(...){...}
-  constructor(firstName, birthyear) {
-    this.firstName = firstName;
+  constructor(fullName, birthyear) {
+    this.fullName = fullName;
     this.birthYear = birthyear;
   }
   // Can declare methods INSIDE the class (prototype) but OUTSIDE the constructor
@@ -125,14 +125,28 @@ class PersonCl {
   /*greet() {
       console.log(`Hey ${this.firstName}`);
   } */
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // setting a property tha already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
 
 PersonCl.prototype.greet = function () {
-  console.log(`Hey ${this.firstName}`);
+  console.log(`Hey ${this.fullName}`);
 };
 
 jessica.greet();
@@ -141,3 +155,26 @@ jessica.greet();
 // 2. Classes are first class citizens
 // 3. Classes are executed in strict mode
 // 4. Usage depends on each programmer
+
+// getters n' setters
+
+const walter = new PersonCl('Walter White', 1965);
+walter.fullName;
+
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+// JS identifies automatically wich method is being called (get or set)
+console.log(account.latest); // getter
+account.latest = 50; // setter
+
+console.log(account.movements);
