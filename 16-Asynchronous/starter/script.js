cfd105d7fef668028bb656df5e8acd8f4076a8a0
Vryzas <vryzas27@gmail.com>
2023-12-03 22:15:07 +0000

    chained promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -112,9 +112,20 @@ const renderCountry = (data, className = '') => {
 // };
 
 const getCountryData = function (country) {
+  // fectch country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+      // fetch neighbour or country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data, 'neighbour');
+    });
 };
 
 getCountryData('portugal');
