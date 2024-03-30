91cc46e849b032ce3e024535c779aa01f7d5aae5
Vitor Costa <vitor.costa@exaud.com>
2024-03-30 17:48:20 +0000

    Ran promises in parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -146,12 +146,12 @@ const getCountryData = function (country) {
 
 ////////////////////////////////////////
 // IMPROVING CODE/ERRORS
-// const getJSON = function (url, errorMessage = 'Something went wrong') {
-//   return fetch(url).then(response => {
-//     if (!response.ok) throw new Error(`${errorMessage} (${response.status})`);
-//     return response.json();
-//   });
-// };
+const getJSON = function (url, errorMessage = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMessage} (${response.status})`);
+    return response.json();
+  });
+};
 
 // const getCountryData = function (country) {
 //   // fectch country 1
@@ -425,7 +425,7 @@ let currentImg;
 
  ////////////////Async/Await Promises////////////////////
  
- const getPosition = function() {
+ /* const getPosition = function() {
   return new Promise(function(resolve, reject) {
    navigator.geolocation.getCurrentPosition(resolve, reject );
   });
@@ -465,7 +465,7 @@ whereAmI()
 .catch(err => console.error(`2: ${err.message} 💥`))
 .finally( () => console.log(`3: Finished getting location.`));
 */
-(async function () {
+/*(async function () {
   try{
     const city  = await whereAmI();
     console.log(`2: ${city}`)
@@ -474,3 +474,34 @@ whereAmI()
   }
   console.log(`3: Finished getting location.`)
 })();
+ */
+//////Promises in parallel/////////
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(
+    //   `https://restcountries.com/v2/name/${c1}`
+    // );
+    // const [data2] = await getJSON(
+    //   `https://restcountries.com/v2/name/${c2}`
+    // );
+    // const [data3] = await getJSON(
+    //   `https://restcountries.com/v2/name/${c3}`
+    // );
+
+    const data = await Promise.all([getJSON(
+      `https://restcountries.com/v2/name/${c1}`
+    ),
+    getJSON(
+      `https://restcountries.com/v2/name/${c2}`
+    ), getJSON(
+      `https://restcountries.com/v2/name/${c3}`
+    )])
+
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+get3Countries('portugal', 'canada', 'australia');
