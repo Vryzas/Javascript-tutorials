358daf8b3cc2c2aefe3ed627f74242b886b2ae13
Vitor Costa <vitor.costa@exaud.com>
2024-06-21 17:37:19 +0100

    Added model for MVC arch. implementation

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,3 +1,5 @@
+import * as model from './model.js';
+
 import icons from 'url:../img/icons.svg';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -33,27 +35,10 @@ const getRecipe = async function () {
     const id = window.location.hash.slice(1);
     if (!id) return;
 
-    // 1. Loading recipe
     renderSpinner(recipeContainer);
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(recipe);
+    // 1. Loading recipe
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
 
     //2. Rendering recipe
     const markup = `
