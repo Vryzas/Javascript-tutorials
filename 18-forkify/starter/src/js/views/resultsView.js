5e98749786611c10d36d3521a66d159cf8175e8f
Vitor Costa <vitor.costa@exaud.com>
2024-06-22 00:36:28 +0100

    Added search results view feature

---
+++ 18-forkify/starter/src/js/views/resultsView.js
@@ -0,0 +1,55 @@
+import icons from 'url:../../img/icons.svg';
+import View from './View.js';
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = `No recipes were found for your query! Please try another search word.`;
+  _message = '';
+
+  renderError(message = this._errorMessage) {
+    const markup = `<div class="error">
+            <div>
+              <svg>
+                <use href="${icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>`;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this._message) {
+    const markup = `<div class="message">
+            <div>
+              <svg>
+                <use href="${icons}#icon-smile"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>`;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+  _generateMarkup() {
+    return this._data.map(this._generateMarkupPreview).join('');
+  }
+
+  _generateMarkupPreview(result) {
+    return `
+        <li class="preview">
+            <a class="preview__link preview__link--active" href="#${result.id}">
+                <figure class="preview__fig">
+                    <img src="${result.image}" alt="${result.title}" />
+                </figure>
+                <div class="preview__data">
+                    <h4 class="preview__title">${result.title}</h4>
+                    <p class="preview__publisher">${result.publisher}</p>
+                </div>
+            </a>
+        </li>
+    `;
+  }
+}
+
+export default new ResultsView();
