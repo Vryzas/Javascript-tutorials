d164859ddf1eae2a135cb330dcac5f3bd736fe2e
Vitor Costa <vitor.costa@exaud.com>
2024-06-26 22:41:15 +0100

    Added bookmarks feature I

--- 18-forkify/starter/src/js/views/recipeView.js
+++ 18-forkify/starter/src/js/views/recipeView.js
@@ -46,6 +46,14 @@ class RecipeView extends View {
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
+  addHandlerAddBookmark(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--bookmark');
+      if (!btn) return;
+      handler();
+    });
+  }
+
   _generateMarkup() {
     return `
       <figure class="recipe__fig">
@@ -95,11 +103,12 @@ class RecipeView extends View {
             </div>
 
             <div class="recipe__user-generated">
-
             </div>
-            <button class="btn--round">
+            <button class="btn--round btn--bookmark">
               <svg class="">
-                <use href="${icons}#icon-bookmark-fill"></use>
+                <use href="${icons}#icon-bookmark${
+      this._data.bookmarked ? '-fill' : ''
+    }"></use>
               </svg>
             </button>
           </div>
