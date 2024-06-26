b601c4110246f4b903449e60fb2fc71761e86a1a
Vitor Costa <vitor.costa@exaud.com>
2024-06-26 23:51:33 +0100

    Moved error and message render function into View.js

--- 18-forkify/starter/src/js/views/View.js
+++ 18-forkify/starter/src/js/views/View.js
@@ -5,11 +5,36 @@ export default class View {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
     this._data = data;
-    this._clear();
     const markup = this._generateMarkup();
 
     if (!render) return markup;
 
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderError(message = this._errorMessage) {
+    const markup = `<div class="error">
+            <div>
+              <svg>
+                <use href="${icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>`;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this._message) {
+    const markup = `<div class="message">
+            <div>
+              <svg>
+                <use href="${icons}#icon-smile"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>`;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
